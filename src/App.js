af98c08f42b2a33be29284ea1e1d45e03663c7ff
Pilar Perez <josepilar21@gmail.com>
2019-04-30 11:51:27 -0700

    Added material ui dependencies and basic toobar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+// import axios from 'axios';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>Test</h1>
-      </header>
+      <AppBar>
+        <Toolbar>
+          <IconButton color="inherit" aria-label="Menu" className="ham-menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            POKEAPI
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
